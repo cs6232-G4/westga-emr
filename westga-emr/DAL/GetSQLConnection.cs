1459e0358e1c4d4d77ce5683b76d7fdf924aef49
Greg Murrell <j_greg_m@comcast.net>
2021-03-16 13:10:03 -0400

    GetSQLConnection

---
+++ westga-emr/DAL/GetSQLConnection.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.SqlClient;
+
+namespace westga_emr.DAL
+{
+    /// <summary>
+    /// This class manages the connection to the cs6232-g4 database on SQL Server
+    /// </summary>
+    public class GetSQLConnection
+    {
+        /// <summary>
+        /// Returns the connection to SQL Server's WestGA-EMR database
+        /// </summary>
+        /// <returns>The SqlConnection to TechSupport DB</returns>
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection("Data Source=localhost;Initial Catalog=cs6232-g4;Integrated Security=True");
+        }
+    }
+}
