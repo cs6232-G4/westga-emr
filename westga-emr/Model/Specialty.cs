3a889af78b18f051adc07b8e93312cbe29be2fd0
Greg Murrell <j_greg_m@comcast.net>
2021-03-16 14:47:26 -0400

    Specialty model

---
+++ westga-emr/Model/Specialty.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace westga_emr.Model
+{
+	/// <summary>
+	/// Models a Specialty in the WestGA-EMR database
+	/// </summary>
+	public class Specialty
+	{
+		public string Name { get; }
+
+		/// <summary>
+		/// Creates a Specialty with the given name
+		/// </summary>
+		/// <param name="name_">Name of the Specialty</param>
+		public Specialty(string name_)
+		{
+			if (string.IsNullOrWhiteSpace(name_))
+            {
+				throw new ArgumentException("name cannot be null, empty, or consist only of white space");
+            }
+			this.Name = name_;
+		}
+	}
+}
