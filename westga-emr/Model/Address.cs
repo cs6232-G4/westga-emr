a7a9b6289d32b2e76d6431c2ea0dca4f35650f6f
Greg Murrell <j_greg_m@comcast.net>
2021-03-16 13:19:29 -0400

    Address model

---
+++ westga-emr/Model/Address.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace westga_emr.Model
+{
+    /// <summary>
+    /// Models an Address in the WestGA-EMR
+    /// </summary>
+    public class Address
+    {
+        public int ID { get; }
+        public string Street { get; }
+        public string City { get; }
+        public string State { get; }
+        public string Zip { get; }
+
+        /// <summary>
+        /// Constructs an instance of Address according to the given data
+        /// </summary>
+        /// <param name="id">SQL Server's ID of the Address</param>
+        /// <param name="street">Street of the Address</param>
+        /// <param name="city">City of the Address</param>
+        /// <param name="state">State of the Address</param>
+        /// <param name="zip">Zip code of the Address</param>
+        public Address(int id, string street, string city, string state, string zip)
+        {
+            if (id < 1)
+            {
+                throw new ArgumentException("id must be greater than zero");
+            }
+            if (string.IsNullOrWhiteSpace(street) || string.IsNullOrWhiteSpace(city)
+                || string.IsNullOrWhiteSpace(state) || string.IsNullOrWhiteSpace(zip))
+            {
+                throw new ArgumentException("street, city, state, and zip cannot be null, empty, or " +
+                    "consist only of white space");
+            }
+            this.ID = id;
+            this.Street = street;
+            this.City = city;
+            this.State = state;
+            this.Zip = zip;
+        }
+    }
+}
