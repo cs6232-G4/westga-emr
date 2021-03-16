bc0731d0dafbbacc2e2a3f0a39058fd4b465bc71
Greg Murrell <j_greg_m@comcast.net>
2021-03-16 14:06:10 -0400

    Lab_Order model

---
+++ westga-emr/Model/Lab_Order.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace westga_emr.Model
+{
+	/// <summary>
+	/// Models a Lab Order in the WestGA-EMR database
+	/// </summary>
+	public class Lab_Order
+	{
+		public long ID { get; }
+		public long VisitID { get; }
+		public DateTime DateOrdered { get; }
+
+		/// <summary>
+		/// Constructs an instance of a Lab Order according to the given parameters
+		/// </summary>
+		/// <param name="id_">Sql Server's id for the Lab Order</param>
+		/// <param name="visitID_">Sql Server's id for the visit associated with the Lab Order</param>
+		/// <param name="dateOrdered_">The date the lab was ordered</param>
+		public Lab_Order(long id_, long visitID_, DateTime dateOrdered_)
+		{
+			this.ID = id_;
+			this.VisitID = visitID_;
+			this.DateOrdered = dateOrdered_;
+		}
+	}
+}
