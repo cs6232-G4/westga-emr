075c770fc2b216ec58accef1c4b01edba2cdb2b8
Greg Murrell <j_greg_m@comcast.net>
2021-03-16 13:57:07 -0400

    Clinical_Administrator model

---
+++ westga-emr/Model/Clinical_Administrator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace westga_emr.Model
+{
+	/// <summary>
+	/// Models a Clinical Administrator in the WestGA-EMR
+	/// </summary>
+	public class Clinical_Administrator
+	{
+		public int ID { get; }
+		public int PersonID { get; }
+		public bool Active { get; }
+
+		/// <summary>
+		/// Constructs a Clinical_Administrator according to the given data
+		/// </summary>
+		/// <param name="id_">Sql Server's id for the Clinical Admin</param>
+		/// <param name="personID_">Sql Server's id for the person who is the Clinical Admin</param>
+		/// <param name="active_">Whether or not the Clinical Admin is active or inactive</param>
+		public Clinical_Administrator(int id_, int personID_, bool active_)
+		{
+			if (id_ < 1 || personID_ < 1)
+            {
+				throw new ArgumentException("id and personID must be greater than zero");
+            }
+			this.ID = id_;
+			this.PersonID = personID_;
+			this.Active = active_;
+		}
+	}
+}
