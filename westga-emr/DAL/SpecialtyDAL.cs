431827f3fbde852733f3fa8841afb017841f20c4
Greg Murrell <j_greg_m@comcast.net>
2021-03-18 14:07:28 -0400

    select *

---
+++ westga-emr/DAL/SpecialtyDAL.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using westga_emr.Model;
+using System.Data.SqlClient;
+
+namespace westga_emr.DAL
+{
+    /// <summary>
+    /// Manages queries to the Specialty table
+    /// </summary>
+    public class SpecialtyDAL
+    {
+        /// <summary>
+        /// Gets a list of all specialties in the db
+        /// </summary>
+        /// <returns>List of all Specialties in db</returns>
+        public static List<Specialty> GetSpecialties()
+        {
+            List<Specialty> specialties = new List<Specialty>();
+            String selectStatement = @"SELECT name FROM Specialty";
+            using (SqlConnection connection = GetSQLConnection.GetConnection())
+            {
+                using (SqlCommand command = new SqlCommand(selectStatement, connection))
+                {
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        int ordName = reader.GetOrdinal("name");
+                        while (reader.Read())
+                        {
+                            specialties.Add(new Specialty(reader.GetString(ordName)));
+                        }
+                    }
+                }
+            }
+            return specialties;
+        }
+    }
+}
