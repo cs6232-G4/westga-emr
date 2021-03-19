3c81b0542b651dd0347487824344fe27cb1db437
Greg Murrell <j_greg_m@comcast.net>
2021-03-19 16:56:04 -0400

    search by user control view complete

---
+++ westga-emr/User Controls/SearchPatient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using westga_emr.Controller;
+
+namespace westga_emr.User_Controls
+{
+    /// <summary>
+    /// User Control for searching for patients
+    /// </summary>
+    public partial class SearchPatient : UserControl
+    {
+        private readonly PatientController controller;
+
+        /// <summary>
+        /// Instantiates the User Control
+        /// </summary>
+        public SearchPatient()
+        {
+            InitializeComponent();
+            this.controller = new PatientController();
+        }
+    }
+}
