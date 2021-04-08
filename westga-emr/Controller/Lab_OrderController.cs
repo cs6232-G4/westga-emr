c2a6ee07f5afb5c0dce33d7a5ff5d72029ceabcb
Greg Murrell <j_greg_m@comcast.net>
2021-04-08 14:32:43 -0400

    can order a new set of lab tests

--- westga-emr/Controller/Lab_OrderController.cs
+++ westga-emr/Controller/Lab_OrderController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using westga_emr.Model;
 using westga_emr.DAL;
+using System;
 
 namespace westga_emr.Controller
 {
@@ -13,6 +14,38 @@ namespace westga_emr.Controller
         public List<Lab_Order> GetLab_Orders()
         {
             return Lab_OrderDAL.GetLab_Orders();
+        }
+
+        /// <summary>
+        /// Orders a new set of labs tests for a Visit
+        /// </summary>
+        /// <param name="order">The Lab_Order itself</param>
+        /// <param name="relation">The relation of Lab_Orders_have_Lab_Tests</param>
+        /// <returns>Whether or not the insertions succeeded</returns>
+        public bool OrderLab(Lab_Order order, Lab_Test[] tests)
+        {
+            if (order == null || tests == null)
+            {
+                throw new ArgumentNullException("order and relation cannot be null");
+            }
+            foreach (Lab_Test test in tests)
+            {
+                if (test.Code == null)
+                {
+                    throw new ArgumentNullException("Lab_Tests must have non-null Codes");
+                }
+            }
+
+            int? labOrderID = Lab_OrderDAL.InsertLab_Order(order);
+            if (labOrderID == null)
+            {
+                return false;
+            }
+            foreach (Lab_Test test in tests)
+            {
+                Lab_Orders_have_Lab_TestsDAL.InsertLab_Orders_have_Lab_Tests(new Lab_Orders_have_Lab_Tests(labOrderID, test.Code, null, null));
+            }
+            return true;
         }
     }
 }
