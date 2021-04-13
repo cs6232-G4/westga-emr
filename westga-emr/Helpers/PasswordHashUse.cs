67dfc08145b365dc1f67f26b5d8df9dc261267bd
Greg Murrell <j_greg_m@comcast.net>
2021-04-13 14:06:22 -0400

    updated for clarity

--- westga-emr/Helpers/PasswordHashUse.cs
+++ westga-emr/Helpers/PasswordHashUse.cs
@@ -8,14 +8,25 @@ namespace westga_emr.Helpers
 {
     public static class PasswordHashUse
     {
-        public static byte[] GetHash(string password)
+        /// <summary>
+        /// Hashes a Password so that it can be saved into a db.
+        /// </summary>
+        /// <param name="password">Password to hash</param>
+        /// <returns>Hash of the password</returns>
+        public static byte[] HashPassword(string password)
         {
             return new PasswordHash(password).ToArray();
         }
 
-        public static bool Verify(byte[] hashedPassword, string password)
+        /// <summary>
+        /// Verify a password against its hash in the db
+        /// </summary>
+        /// <param name="hashedFromDB">Hash obtained from a db</param>
+        /// <param name="password">Password to verify</param>
+        /// <returns></returns>
+        public static bool VerifyPassword(byte[] hashFromDB, string password)
         {
-            return new PasswordHash(hashedPassword).Verify(password);
+            return new PasswordHash(hashFromDB).Verify(password);
         }
     }
 }
