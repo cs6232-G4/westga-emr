e25c47a0841e86f514d8b4678381c6cb39f96cff
Greg Murrell <j_greg_m@comcast.net>
2021-04-13 13:53:54 -0400

    PasswordHashUse for easy use of PasswordHash class

---
+++ westga-emr/Helpers/PasswordHashUse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace westga_emr.Helpers
+{
+    public static class PasswordHashUse
+    {
+        public static byte[] GetHash(string password)
+        {
+            return new PasswordHash(password).ToArray();
+        }
+
+        public static bool Verify(byte[] hashedPassword, string password)
+        {
+            return new PasswordHash(hashedPassword).Verify(password);
+        }
+    }
+}
