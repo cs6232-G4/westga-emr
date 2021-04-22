36b553e05607afd5a8ba1c3822766df22050cf1d
Greg Murrell <j_greg_m@comcast.net>
2021-04-22 16:09:02 -0400

    model, dal, sql script, controller for report

---
+++ westga-emr/Controller/ReportController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using westga_emr.DAL;
+using westga_emr.Model;
+
+namespace westga_emr.Controller
+{
+    /// <summary>
+    /// Controller for Report DAL and Model
+    /// </summary>
+    public class ReportController
+    {
+        /// <see cref="ReportDAL.GetMostPerformedTestsDuringDates(DateTime, DateTime)"/>
+        public static Report GetMostPerformedTestsDuringDates(DateTime startDate, DateTime endDate)
+        {
+            if (startDate == null || endDate == null)
+            {
+                throw new ArgumentNullException("startDate and endDate cannot be null");
+            }
+            if (DateTime.Compare(startDate, endDate) > 0)
+            {
+                throw new ArgumentException("endDate cannot be less than startDate");
+            }
+            return ReportDAL.GetMostPerformedTestsDuringDates(startDate, endDate);
+        }
+    }
+}
---
+++ westga-emr/Model/Report.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace westga_emr.Model
+{
+    public class Report
+    {
+        public int LabCode { get; }
+        public string LabName { get; }
+        public int NumberOfTests { get; }
+        public int TotalTests { get; }
+        public decimal PercentageOfTotal { get; }
+        public int NumberOfNormalResults { get; }
+        public int NumberOfAbnormalResults { get; }
+        public decimal PercentageDoneOnTwentyYearOldsDuringThisTime { get; }
+        public decimal PercentageDoneOnThirtyYearOldsDuringThisTime { get; }
+        public decimal PercentageDoneOnOtherAgesOldsDuringThisTime { get; }
+
+        public Report(int LabCode_, string LabName_, int NumberOfTests_, int TotalTests_, decimal PercentageOfTotal_,
+            int NumberOfNormalResults_, int NumberOfAbnormalResults_, decimal PercentageDoneOnTwentyYearOldsDuringThisTime_,
+            decimal PercentageDoneOnThirtyYearOldsDuringThisTime_, decimal PercentageDoneOnOtherAgesOldsDuringThisTime_)
+        {
+            if (LabCode_ < 1)
+            {
+                throw new ArgumentException("code must be greater than zero");
+            }
+            if (string.IsNullOrWhiteSpace(LabName_))
+            {
+                throw new ArgumentException("name cannot be null, empty, or consist only of white space");
+            }
+            if (NumberOfTests_ < 0 || NumberOfNormalResults_ < 0 || NumberOfAbnormalResults_ < 0)
+            {
+                throw new ArgumentException("cannot have negative value for number of tests, normal results, or abnormal results");
+            }
+            if (PercentageOfTotal_ < 0 || PercentageDoneOnTwentyYearOldsDuringThisTime_ < 0 || 
+                PercentageDoneOnThirtyYearOldsDuringThisTime_ < 0 || PercentageDoneOnOtherAgesOldsDuringThisTime_ < 0)
+            {
+                throw new ArgumentException("cannot have negative value for percentages");
+            }
+            this.LabCode = LabCode_;
+            this.LabName = LabName_;
+            this.NumberOfTests = NumberOfTests_;
+            this.TotalTests = TotalTests_;
+            this.PercentageOfTotal = PercentageOfTotal_;
+            this.NumberOfNormalResults = NumberOfNormalResults_;
+            this.NumberOfAbnormalResults = NumberOfAbnormalResults_;
+            this.PercentageDoneOnTwentyYearOldsDuringThisTime = PercentageDoneOnTwentyYearOldsDuringThisTime_;
+            this.PercentageDoneOnThirtyYearOldsDuringThisTime = PercentageDoneOnThirtyYearOldsDuringThisTime_;
+            this.PercentageDoneOnOtherAgesOldsDuringThisTime = PercentageDoneOnOtherAgesOldsDuringThisTime_;
+        }
+    }
+}
